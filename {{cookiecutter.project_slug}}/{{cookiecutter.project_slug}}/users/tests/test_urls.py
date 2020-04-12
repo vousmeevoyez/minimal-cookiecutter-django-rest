a3a47627e739b52c2ce4d1c45e85c328f96ad041
Kelvin <kelvindsmn@gmail.com>
2020-04-12 15:51:13 +0700

    more fix

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/tests/test_urls.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/tests/test_urls.py
@@ -8,19 +8,19 @@ pytestmark = pytest.mark.django_db
 def test_user_list():
     assert (
         reverse("user-list")
-        == "api/users/"
+        == "/api/users/"
     )
-    assert resolve("/users/").view_name == "users:list"
+    assert resolve("/api/users/").view_name == "users:list"
 
 
 def test_user_me():
-    assert reverse("user-me") == "api/users/me/"
-    assert resolve("/users/me/").view_name == "users:me"
+    assert reverse("user-me") == "/api/users/me/"
+    assert resolve("/api/users/me/").view_name == "users:me"
 
 
 def test_user_detail(user: User):
     assert (
         reverse("user-detail", kwargs={"username": user.username})
-        == f"api/users/{user.username}/"
+        == f"/api/users/{user.username}/"
     )
-    assert resolve(f"api/users/{user.username}/").view_name == "users:detail"
+    assert resolve(f"/api/users/{user.username}/").view_name == "users:detail"
