e2be7057faf2db429f38b6e724951d19707027a0
Kelvin <kelvindsmn@gmail.com>
2020-04-12 15:46:18 +0700

    fix unittest

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/tests/test_urls.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/tests/test_urls.py
@@ -3,22 +3,23 @@ from django.urls import resolve, reverse
 
 from {{ cookiecutter.project_slug }}.users.models import User
 
-pytestmark = pytest.mark.django_db
 
-
-def test_detail(user: User):
+def test_user_list():
     assert (
-        reverse("users:detail", kwargs={"username": user.username})
+        reverse("user-list")
+        == f"/users/"
+    )
+    assert resolve(f"/users/").view_name == "users:list"
+
+
+def test_user_me():
+    assert reverse("user-me") == "/users/me/"
+    assert resolve("/users/me/").view_name == "users:me"
+
+
+def test_user_detail(user: User):
+    assert (
+        reverse("user-detail", kwargs={"username": user.username})
         == f"/users/{user.username}/"
     )
     assert resolve(f"/users/{user.username}/").view_name == "users:detail"
-
-
-def test_update():
-    assert reverse("users:update") == "/users/~update/"
-    assert resolve("/users/~update/").view_name == "users:update"
-
-
-def test_redirect():
-    assert reverse("users:redirect") == "/users/~redirect/"
-    assert resolve("/users/~redirect/").view_name == "users:redirect"
