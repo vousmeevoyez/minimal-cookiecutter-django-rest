86e159001beeda197c7d6e6d8f1859745b92130d
Kelvin <kelvindsmn@gmail.com>
2020-04-12 15:29:36 +0700

    fix routes

--- {{cookiecutter.project_slug}}/config/api_router.py
+++ {{cookiecutter.project_slug}}/config/api_router.py
@@ -1,8 +1,5 @@
 from {{ cookiecutter.project_slug }}.users.urls import urlpatterns as \
     users_url_patterns
 
-app_name = "api"
 urlpatterns = []
-urlpatterns += [
-    users_url_patterns
-]
+urlpatterns += users_url_patterns
